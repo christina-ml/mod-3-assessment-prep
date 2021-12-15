6645eeb8b8d67b4218b3a7243f88bc9dca152673
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-15 11:30:25 -0500

    going to make Home into its own component

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" component={Home} />
+        {/* <Route path="/" component={Shows} /> */}
       </Switch>
     </Router>
   );
