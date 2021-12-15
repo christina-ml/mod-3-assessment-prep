5b731054df1423ecf2131b02398261dfffae2ef5
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-15 11:53:34 -0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { 
   BrowserRouter as Router, 
+  Link, 
   Route,
   Switch 
 } from 'react-router-dom';
@@ -10,6 +11,14 @@ import Shows from './components/Shows';
 function App() {
   return (
     <Router>
+
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/shows">Shows</Link></li>
+        </ul>
+      </nav>
+
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/shows" component={Shows} />
