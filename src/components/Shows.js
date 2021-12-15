2911df5482b69cc4ae759168eedf3cb2ffb8c42f
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-15 12:30:09 -0500

    made a fetch, componentDidMount, and put an empty data array in our state

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -5,6 +5,7 @@ class Shows extends Component {
       super();
       this.state = {
           display: true,
+          data: [],
       }
     }
   
@@ -13,12 +14,41 @@ class Shows extends Component {
     // Practice inputs - onClick, onSelect, onCheckbox
     // Storing different things ins tate: arrays, objects, booleans
 
+    handleFetch=()=>{
+        fetch("https://pursuit-tv-show.herokuapp.com/api/shows")
+            .then(res=>res.json())
+            .then((data)=>{
+                console.log(data);
+            })
+    }
+
+    handleShowHide=(e)=>{
+        this.setState({
+            display: e.target.checked,
+        })
+    }
+
+    componentDidMount(){
+        this.handleFetch();
+    }
+
     render(){
       return(
         <div>
             <h1>Shows Page</h1>
+            Show/Hide
+            <input 
+                type="checkbox" 
+                onChange={this.handleShowHide}
+                checked={this.state.display}
+             />
+
             { this.state.display && 
-                <div>Thing to show/hide</div>
+                <div>Thing is shown</div>
+            }
+
+            { !this.state.display && 
+                <div>Thing is hidden</div>
             }
 
         </div>
