bb637ef4d532aced06b13bbb2f6c1737c8449bb0
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-15 11:42:29 -0500

    added exact path to home Route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/shows" component={Shows} />
       </Switch>
     </Router>
